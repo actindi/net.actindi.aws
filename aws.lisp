aa836d2911c9e12797977a29fec0b6295895e072
Yoshinori Tahara <read.eval.print@gmail.com>
2012-04-07 17:30:02 +0900

    指定したインスタンスの EBS のスナップショットをとって AMI を作る make-ami-from-instance を実装。

--- aws.lisp
+++ aws.lisp
@@ -51,3 +51,39 @@
 
 (def-commands "~/local/opt/AutoScaling/bin/")
 ;;(as-cmd)
+
+(defun make-ami-from-instance (instance-id)
+  "指定したインスタンスの EBS のスナップショットをとって AMI を作る。"
+  (let (kernel-id)
+    (labels ((volume-id (instance-id)
+               (multiple-value-bind (ok exit-code stdout)
+                   (ec2-describe-instances :show-empty-fields instance-id)
+                 (declare (ignore ok exit-code))
+                 (ppcre:register-groups-bind ($kernel-id $volume-id)
+                     ((ppcre:create-scanner
+                       "INSTANCE\\s+\\S+\\s+\\S+\\s+\\S+\\s+\\S+\\s+\\S+\\s+\\S+\\s+\\S+\\s+\\S+\\s+\\S+\\s+\\S+\\s+\\S+\\s+(\\S+).*BLOCKDEVICE\\s+\\S+\\s+(\\S+)"
+                       :single-line-mode t)
+                      stdout)
+                   (setf kernel-id $kernel-id)
+                   $volume-id)))
+             (make-snapshot (volume-id)
+               (multiple-value-bind (ok exit-code stdout)
+                   (ec2-create-snapshot
+                    :show-empty-fields
+                    :d (format nil "\"~a(app) ~a\"" instance-id volume-id)
+                    volume-id)
+                 (declare (ignore ok exit-code))
+                 (ppcre:register-groups-bind (snapshot-id)
+                     ("SNAPSHOT\\s+(\\S+)" stdout)
+                   snapshot-id)))
+             (make-ami (snapshot-id)
+               (ec2-register
+                :n "\"app-`date +\\%Y-\\%m-\\%d-\\%H-\\%M-\\%S`\""
+                :d "\"app image\""
+                :root-device-name "/dev/sda1"
+                :a "x86_64"
+                :kernel kernel-id
+                :b (format nil "/dev/sda=~a" snapshot-id))))
+      (make-ami
+       (make-snapshot
+        (volume-id instance-id))))))
